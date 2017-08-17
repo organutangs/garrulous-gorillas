99e99e61ad0fc3955fa3cdd249cc0bc4478b5eca
Michael Lam <mmmikeal@gmail.com>
2017-08-17 19:59:05 -0400

    missed a file for some reason

--- server/models/debateArg.js
+++ server/models/debateArg.js
@@ -8,6 +8,7 @@ const DebateArgSchema = mongoose.Schema({
   debateSide: String,
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   activeDebate: { type: mongoose.Schema.Types.ObjectId, ref: 'Debate' },
+  voteDate: [{vote:Number, date:Date}]
 });
 
 const DebateArg = module.exports = mongoose.model('DebateArg', DebateArgSchema);
@@ -34,7 +35,9 @@ module.exports.addVoteForArgumentById = (id, numOfVotes, callback) => {
 module.exports.addVoteForArgumentByBody = (argument, numOfVotes, callback) => {
   DebateArg.findOneAndUpdate(
     { body: argument },
+    { $push: { voteDate: {vote: 1, date: Date.now} } },
     { $inc: {'votes': numOfVotes} },
+
   callback);
 };
 
