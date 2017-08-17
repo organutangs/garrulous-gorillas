9c32ec0e1e3b8d74f5f1caa8214fdc9bb6793b29
Michael Lam <mmmikeal@gmail.com>
2017-08-17 19:55:45 -0400

    finished minimum filter by hot arguments functionality and schema. Next I will add logarithmic weight for votes that were done in the past few days versus weeks versus months to sort by

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,10 +1,31 @@
-//everytime a vote is triggered add username to vote
-//add timeStamp to vote
-//if already voted, dont let them vote from the already made boolean
-//
-
+//add timestamp for vote
 exports.sortArgsByHot = function(data) {
-return data;
+  //set time to today's date minus 30 days.
+  //we want to use only votes from the last 30 days.
+  var time = new Date();
+  time.setDate(time.getDate()-30);
+  //console.log("check if time is 30days ago", time);
+
+  //helper function sums up votes
+  var voteTotal = (data)=> {
+    return data.reduce((acc, item)=> {
+      return acc + item.vote;
+    });
+  };
+
+  //recentVotes is an array of all arguments that have been voted on in the last 30days
+  let recentVotes = data.voteData.filter((item)=> {
+    return new Date(item.date) > new Date(time);
+  });
+
+  //total votes this argument has received in 30 days
+  var sorted = recentVotes.sort((a,b)=> {
+    return b.voteTotal(b.voteDate) - a.voteTotal(a.voteDate);
+  });
+
+  return sorted;
+  //tomorrow i will add 10x logarithm weights for votes within a few days vs weeks vs months
+
 }
 
 exports.sortArgsByNew = function(data) {
