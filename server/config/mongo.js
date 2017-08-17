623516e2358ec29309a228426c5d2b76b3dec5a7
Nick <nicholaspowel@users.noreply.github.com>
2017-08-17 15:35:43 -0400

    Update mongo.js

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // database: 'mongodb://gorilla:gorilla@ds123752.mlab.com:23752/debateapp',
-  database: 'mongodb://localhost/gorillas',
+  database: 'mongodb://gorilla:gorilla@ds123752.mlab.com:23752/debateapp',
   secret: 'topsecret'
 };
