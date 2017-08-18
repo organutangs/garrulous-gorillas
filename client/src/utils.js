c3d400404d6a910ab3faa27d83dea01d65254616
Michael Lam <mmmikeal@gmail.com>
2017-08-18 17:10:07 -0400

    Hot Logarithm filter feature working! removed console.logs and code cleanup

--- client/src/utils.js
+++ client/src/utils.js
@@ -2,29 +2,50 @@
 exports.sortArgsByHot = function(data) {
   //set time to today's date minus 30 days.
   //we want to use only votes from the last 30 days.
-  var time = new Date();
-  time.setDate(time.getDate()-30);
-  //console.log("check if time is 30days ago", time);
+  var timeMonth = new Date();
+  var timeWeek = new Date();
+  var timeThreeDays = new Date();
 
-  //helper function sums up votes
-  var voteTotal = (data)=> {
-    return data.reduce((acc, item)=> {
-      return acc + item.vote;
-    });
-  };
-  console.log('***This is Data*** ', data);
-  //recentVotes is an array of all arguments that have been voted on in the last 30days
-  let recentVotes = data.filter((item)=> {
-    return new Date(item.voteDate.date) > new Date(time);
+  timeThreeDays.setDate(timeMonth.getDate() - 3); //the date range from today to 3 days ago
+  timeWeek.setDate(timeThreeDays.getDate() - 7); //the date range from 3 days ago to 7 days ago
+  timeMonth.setDate(timeWeek.getDate() - 23); //the date range from 7 days ago to 30 days ago
+
+  var sorted = data.sort((a, b)=> {
+    //helper function sums up votes
+    const voteTotal = (data1)=> {
+      return data1.reduce((acc, item)=> {
+        return acc + item.vote;
+      }, 0);
+    };
+
+    //pass in a time to filter out votes that were older than that time
+    const filterByTime = (data1, time) => {
+      return data1.voteDate.filter((item)=> {
+        return new Date(item.date) > time;
+      });
+    };
+
+    //total votes by time period
+    let votesFromThreeDaysAgoA = voteTotal(filterByTime(a, timeThreeDays));
+    let votesFromWeekAgoA = voteTotal(filterByTime(a, timeWeek));
+    let votesFromMonthAgoA = voteTotal(filterByTime(a, timeMonth));
+
+    let votesFromThreeDaysAgoB = voteTotal(filterByTime(b, timeThreeDays));
+    let votesFromWeekAgoB = voteTotal(filterByTime(b, timeWeek));
+    let votesFromMonthAgoB = voteTotal(filterByTime(b, timeMonth));
+
+    //10x logarithm weights for votes within a few days vs weeks vs months
+    let logTotalVotesA = Math.log10(votesFromThreeDaysAgoA) + Math.log10(Math.log10(votesFromWeekAgoA)) + Math.log10(Math.log10(Math.log10(votesFromMonthAgoA)));
+    let logTotalVotesB = Math.log10(votesFromThreeDaysAgoB) + Math.log10(Math.log10(votesFromWeekAgoB)) + Math.log10(Math.log10(Math.log10(votesFromMonthAgoB)));
+
+    //append weights
+    a["logVotes"] = logTotalVotesA;
+    b["logVotes"] = logTotalVotesB;
+
+    return b.logVotes - a.logVotes;
   });
-
-  //total votes this argument has received in 30 days
-  var sorted = recentVotes.sort((a,b)=> {
-    return b.voteTotal(b.voteDate) - a.voteTotal(a.voteDate);
-  });
-
   return sorted;
-  //tomorrow i will add 10x logarithm weights for votes within a few days vs weeks vs months
+
 
 }
 
@@ -36,6 +57,7 @@ exports.sortArgsByNew = function(data) {
   console.log('sort by new', sorted);
   return sorted;
 }
+//example of UTC date
 //2017-08-04T16:03:38.480Z
 
 exports.sortArgsByVote = function(data) {
--- server/models/debateArg.js
+++ server/models/debateArg.js
@@ -8,7 +8,7 @@ const DebateArgSchema = mongoose.Schema({
   debateSide: String,
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   activeDebate: { type: mongoose.Schema.Types.ObjectId, ref: 'Debate' },
-  voteDate: [{vote:Number, date:{type:Date, default:Date.now}}]
+  voteDate: [{vote:Number, date:{ type: Date, default: Date.now }}]
 });
 
 const DebateArg = module.exports = mongoose.model('DebateArg', DebateArgSchema);
@@ -32,6 +32,7 @@ module.exports.addVoteForArgumentById = (id, numOfVotes, callback) => {
 };
 
 // Find the active debate by argumentText and add to number of votes for a particular side
+//also push a {vote:1, date:now} tupule for record keeping
 module.exports.addVoteForArgumentByBody = (argument, numOfVotes, callback) => {
   DebateArg.findOneAndUpdate(
     { body: argument },
